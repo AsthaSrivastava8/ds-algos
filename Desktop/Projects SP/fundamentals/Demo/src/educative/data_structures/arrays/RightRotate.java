92b1c7e26581ee06df2f56485953d0e9e1f4c385
Astha <srivastavastha811@gmail.com>
2021-05-05 01:02:15 +0530

    right-rotate

--- Desktop/Projects SP/fundamentals/Demo/src/educative/data_structures/arrays/RightRotate.java
+++ Desktop/Projects SP/fundamentals/Demo/src/educative/data_structures/arrays/RightRotate.java
@@ -1,7 +1,19 @@
 package educative.data_structures.arrays;
 
+import java.util.Arrays;
+
 public class RightRotate {
     public static void main(String[] args) {
+        int[] arr = {3, 6, 5, 4};
+        int nextLoop = arr[0];
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            int temp = nextLoop;
+            nextLoop = arr[i + 1];
+            arr[i + 1] = temp;
++        }
+        arr[0] = nextLoop;
+
+        Arrays.stream(arr).forEach(System.out::println);
     }
 }
